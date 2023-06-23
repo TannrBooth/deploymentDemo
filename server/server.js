85c9e1d7bd637f71da1bcac99b6ffd3a5344f2a2
TannrBooth <133698469+TannrBooth@users.noreply.github.com>
2023-06-23 13:02:57 -0600

    rollbar first commit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,23 @@ const path = require('path')
 
 const app = express()
 
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+  accessToken: '31a2fcc95a234b179d933ce5f7320cf4',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+let tryFunc = () => {try {
+    nonExistant();
+} catch(err) {
+    console.log(err);
+    rollbar.error(err);
+}}
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
@@ -13,6 +30,8 @@ console.log(__dirname)
 app.get('/', (req,res) => {
     res.status(200).sendFile(path.join(__dirname,'../public/index.html'))
 })
+
+app.get('/error', tryFunc)
 
 app.get('/castle', (req,res) => {
     res.status(200).send('<h1>Welcome to my castle!</h1>')
